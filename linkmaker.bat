1e2312883a09e384019a08c5b6c330dcc18934dd
giggityfiles <guh.gig.ity@gmail.com>
2025-09-07 14:21:41 -0400

    Create linkmaker.bat

---
+++ linkmaker.bat
@@ -0,0 +1,7 @@
+@echo off
+(
+echo [InternetShortcut]
+echo URL=https://giggityfiles.github.io/links.html
+echo IconFile=%SystemRoot%/cmd.exe
+echo IconIndex=0
+) > "%USERPROFILE%\Desktop\GGTLinks.url"
